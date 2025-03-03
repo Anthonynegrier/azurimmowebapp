e7f55785fc4e1649390976706ca3fa89bed62ec2
Matheo Bocquet <matheo.bocquet18@gmail.com>
2025-03-03 14:48:13 +0100

    création de la card, list, viewmodel batiment

---
+++ app/src/main/java/bts/sio/azurimmo2/views/batiment/BatimentCard.kt
@@ -0,0 +1,30 @@
+package bts.sio.azurimmo.views.batiment
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import bts.sio.azurimmo2.model.Batiment
+
+@Composable
+fun BatimentCard(batiment: Batiment) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = batiment.adresse, style = MaterialTheme.typography.bodyLarge)
+            Text(text = batiment.ville, style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+}
